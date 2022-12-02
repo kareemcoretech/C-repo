040c5e7c19259e2868736ba7719547dfe1c74049
unknown <Mohmed.rabea7473@gmail.com>
2022-12-02 18:09:36 +0200

    Rabea first commit

---
+++ Mohamed_Rabea/Assignments/Assignment 1/problem1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int x,y, result;
+	printf("Enter two Intergers :");
+	scanf("%d",&x);
+	scanf("%d",&y);
+	result=((x+y)*3)-10;
+	printf("the Result =%d ",result);
+}
