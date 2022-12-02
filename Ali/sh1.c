c883a136e84970adf9d58f87681741f2dd696220
unknown <3li27med@gmail.com>
2022-12-02 18:33:38 +0200

    ali first commit

---
+++ Ali/sh1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int r,x,y;
+		printf("Enter number 1: ");
+		scanf("%d",&x);
+		printf("Enter number 1: ");
+		scanf("%d",&y);
+		r=((x+y)*3)-10;
+		printf("%d",r);
+}
