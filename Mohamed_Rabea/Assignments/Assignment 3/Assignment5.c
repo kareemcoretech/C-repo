2f1822fe8de5dd2771b2a7ae47b78be87f085f28
unknown <Mohmed.rabea7473@gmail.com>
2022-12-07 22:28:01 +0200

    Rabea

---
+++ Mohamed_Rabea/Assignments/Assignment 3/Assignment5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+	int i,j;
+	for (i=0; i<5;i++){
+	printf("\n");
+
+		for(j=0; j<=i; j++){
+			printf ("*  ");
+			
+		}
+	}
+
+}
---
+++ Mohamed_Rabea/Assignments/Assignment 3/Assignment7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+	int i,j,k;
+	for (i=0; i<5;i++){
+	
+	    for(j=5; j>=i; j--){
+			printf (" ");
+			
+		}
+		for(j=0; j<=i; j++){
+			printf ("* ");
+			
+		}
+		printf("\n");
+		
+	}
+
+}
