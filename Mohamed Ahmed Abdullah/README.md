bcd35512c1179f6c4506e6c00e6bb80b87464f97
MohdFarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2022-12-13 17:11:28 +0200

    Upload Day02 - Lab 6,7

--- Mohamed Ahmed Abdullah/README.md
+++ Mohamed Ahmed Abdullah/README.md
@@ -1,7 +1,6 @@
 # C Repo for IMT
 
-###### Mohamed Ahmed Abdullah Mahmoud 
-
+##### Mohamed Ahmed Abdullah Mahmoud 
 ---
 
 ## Contents
@@ -9,3 +8,17 @@
 - Assignments for lectures
 - Assignments
 - Labs
+
+## Tasks
+
+- Day 3 labs
+- Day 4 labs
+- Day 5 lab 7
+
+- Assignment 3
+- Assignment 4
+- Assignment 5
+
+- Lectrue 3 Assignment
+- Lecture 4 Assignment
+- Lecture 5 Assignment
---
+++ Mohamed Ahmed Abdullah/labs/Day02/lab7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	int number1;
+    int number2;
+    int number3;
+
+    printf("Enter number 1: ");
+    scanf("%d", &number1);
+    printf("Enter number 2: ");
+    scanf("%d", &number2);
+    printf("Enter number 3: ");
+    scanf("%d", &number3);
+
+    int max;
+    if (number1 > number2 && number1 > number3)
+    {
+        max = number1;
+    }else if (number2 > number1 && number2 > number3)
+    {
+        max = number2;
+    }else if (number3 > number2 && number3 > number1)
+    {
+        max = number3;
+    }
+
+    printf("Maximum number is %d", max);
+    
+	
+	return 0;
+}
