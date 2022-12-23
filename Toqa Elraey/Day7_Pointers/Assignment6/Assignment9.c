cf9fbcea5a5cdbac23162fcbea9a68321b942e8b
Toto <toqaelraey512@gmail.com>
2022-12-23 12:53:32 +0200

    edited assignment 9 in assignment 6

--- Toqa Elraey/Day7_Pointers/Assignment6/Assignment9.c
+++ Toqa Elraey/Day7_Pointers/Assignment6/Assignment9.c
@@ -1,27 +1,31 @@
 /*
 Author: Toqa Amir Elraey
-Version: 3
+Version: 1
 Task: Write a function to concatenate two strings.
-Date: 21/12/2022
+Date: 23/12/2022
 */
 
 #include <stdio.h>
 
 int LengthOfArray(char Array[]);
-void Concatenating(char Array1[], char Array2[]);
+void Concatenating(char Array1[], char Array2[], char Array3[], int size1, int size2);
 
 int main(){
 	char str1[100];
 	char str2[100];
-	printf("Please enter your STRING: ");
+	printf("Please enter your first STRING: ");
 	scanf("%s", str1);
-	printf("Please enter your STRING: ");
+	printf("Please enter your second STRING: ");
 	scanf("%s", str2);
 	
 	int size1 = LengthOfArray(str1);
 	int size2 = LengthOfArray(str2);
 	
-	Concatenating(str1, str2);
+	//declaring a new array to concatenate the two strings:
+	char str3[100];
+	
+	Concatenating(str1, str2, str3, size1, size2);
+	printf("%s", str3);
 	
 }
 
@@ -32,7 +36,12 @@ int LengthOfArray(char Array[]){
 	return count;
 }
 
-void Concatenating(char Array1[], char Array2[]){
-	printf("%s %s", Array1, Array2);
+void Concatenating(char Array1[], char Array2[], char Array3[], int size1, int size2){
+	for(int i = 0; i < size1; i++){
+		Array3[i] = Array1[i];
+	}
+	for(int i = 0; i < size2; i++){
+		Array3[i + size1] = Array2[i];
+	}
 }
 
